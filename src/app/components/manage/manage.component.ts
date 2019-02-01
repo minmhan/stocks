a4d0d159a533d05b64c3d1742b3327cd69bc75ae
Min Min <min.han@stradegi.com>
2019-02-01 11:13:25 +0800

    'init'

---
+++ src/app/components/manage/manage.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { StocksService } from '../../services/stocks.service';
+
+@Component({
+  selector: 'app-manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.css']
+})
+export class ManageComponent implements OnInit {
+  symbols: Array<string>;
+  stock: string;
+
+  constructor(private service: StocksService) { 
+    this.symbols = service.get();
+  }
+
+  ngOnInit() {
+  }
+
+  add() {
+    this.symbols = this.service.add(this.stock.toUpperCase());
+  }
+
+  remove(symbol) {
+    this.symbols = this.service.remove(this.service.remove(symbol));
+  }
+}
